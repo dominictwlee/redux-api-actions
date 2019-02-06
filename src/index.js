7ab70b88ac5cb7cd3d243ecfb54c8f5b1141a69e
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-02-06 22:46:42 +0000

    feat: 🎸 Create middleware for handling api actions
    Middleware and utility function for handling async api actions

---
+++ src/index.js
@@ -0,0 +1,34 @@
+export const apiActions = ({ dispatch, getState }) => next => action => {
+  const { type, meta, payload } = action;
+
+  if (!meta || !meta.apiCall) {
+    return next(action);
+  }
+
+  if (typeof meta.apiCall !== 'function') {
+    throw new Error('Expected meta.apiCall to be a function');
+  }
+
+  const actionSuffix = /REQUEST$/;
+  const successType = type.replace(actionSuffix, 'SUCCESS');
+  const failureType = type.replace(actionSuffix, 'FAILURE');
+
+  if (payload) {
+    dispatch({ type, payload });
+  } else {
+    dispatch({ type });
+  }
+
+  return meta
+    .apiCall()
+    .then(res => dispatch({ type: successType, payload: res }))
+    .catch(err => dispatch({ type: failureType, payload: err, error: true }));
+};
+
+export function createAsyncActionTypes(reducerKey, type) {
+  const requestActionType = `${reducerKey}/${type}_REQUEST`;
+  const successActionType = `${reducerKey}/${type}_SUCCESS`;
+  const failureActionType = `${reducerKey}/${type}_FAILURE`;
+
+  return [requestActionType, successActionType, failureActionType];
+}
