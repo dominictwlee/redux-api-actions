03d6fdd30d9e095f264c2db5d6957723aba88b31
Ismail Alarmouti <alarmouti.ismail@gmail.com>
2019-04-08 17:10:03 +0100

    fix: added @types/redux module to properly type the apiActions parameter as the Store type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
+import { Store } from 'redux';
+
 export function createAsyncActionTypes(reducerKey: string, type: string): string[];
-export interface middleWareInput {
-  dispatch(action: object): any;
-  getState(): any;
-}
-export function apiActions(middleWareInput): any;
+
+export function apiActions(Store): any;
