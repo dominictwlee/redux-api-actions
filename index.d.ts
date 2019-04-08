677ad4ac40dbaa3034c6630c09e6b4da3e53ae22
Ismail Alarmouti <alarmouti.ismail@gmail.com>
2019-04-08 16:23:09 +0100

    chore: added apiActions type declaration export from root index file

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,6 @@
 export function createAsyncActionTypes(reducerKey: string, type: string): string[];
+export interface middleWareInput {
+  dispatch(action: object): any;
+  getState(): any;
+}
+export function apiActions(middleWareInput): any;
