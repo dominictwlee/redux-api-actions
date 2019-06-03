46d0b4891ece0eaf0a975bd7dd2ea246b9531257
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-06-03 15:59:58 +0100

    fix: typescript def

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ import { Store } from 'redux';
 
 export function createAsyncActionTypes(reducerKey: string, type: string): string[];
 
-export function apiActions(Store): any;
+export function apiActions(store: Store): any;
