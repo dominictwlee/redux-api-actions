f7659432a9a740865a9585e10b883c97cef99374
Ismail Alarmouti <alarmouti.ismail@gmail.com>
2019-06-13 11:00:46 +0100

    fix: changed middleware typescript def to match latest redux update

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-import { Store } from 'redux';
+import { Store, Middleware } from 'redux';
 
 export function createAsyncActionTypes(reducerKey: string, type: string): string[];
 
-export function apiActions(store: Store): any;
+export const apiActions: Middleware<any, {}, any>;
