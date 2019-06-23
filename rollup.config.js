938db00a43a28a739473991f973b8af2c0cee3b1
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-06-23 17:05:13 +0100

    fix: Include normalizr but only es module

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import pkg from './package.json';
 import babel from 'rollup-plugin-babel';
 import analyze from 'rollup-plugin-analyzer';
 import gzipPlugin from 'rollup-plugin-gzip';
+import resolve from 'rollup-plugin-node-resolve';
 import minify from 'rollup-plugin-babel-minify';
 
 export default [
@@ -9,13 +10,13 @@ export default [
     input: 'src/index.js',
     plugins: [
       analyze(),
+      resolve({ modulesOnly: true }),
       babel({
         exclude: 'node_modules/**',
       }),
       minify(),
       gzipPlugin(),
     ],
-    external: ['normalizr'],
     output: [
       { file: pkg.main, format: 'cjs', sourcemap: true },
       { file: pkg.module, format: 'es', sourcemap: true },
