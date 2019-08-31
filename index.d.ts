f4bf82b5b39b1986b098e7f66d25f6a011b273d9
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-08-31 21:34:00 +0100

    fix: Remove default param

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ import { Store, Middleware } from 'redux';
 
 export const apiActions: Middleware<any, {}, any>;
 
-export function stateChangeMap<S, A, F>(state: S, action: A = {}, fnMap: any = {}): S;
+export function stateChangeMap<S, A, F>(state: S, action: A, fnMap: any): S;
--- src/stateChangeMap.js
+++ src/stateChangeMap.js
@@ -1,6 +1,6 @@
 import { META } from './constants';
 
-export default function stateChangeMap(state, action = {}, fnMap = {}) {
+export default function stateChangeMap(state, action, fnMap) {
   if (!action.meta || !action.meta[META.lifecycle]) {
     throw new Error(
       `${action.type}: Meta key is either missing, or not valid for use with redux-api-actions handler`
