44b82ebd7158ea0ebdbb99d1853d3260066605ce
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-08-31 21:22:17 +0100

    fix: Add stateChangeMap type def

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 import { Store, Middleware } from 'redux';
 
 export const apiActions: Middleware<any, {}, any>;
+
+export function stateChangeMap<S, A, F>(state: S, action: A = {}, fnMap: any = {}): S;
