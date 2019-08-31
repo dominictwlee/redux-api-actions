aa5673f2f04ce21e74815c60982a525e2ae39475
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-08-31 18:05:51 +0100

    fix: babel config for test env

--- babel.config.js
+++ babel.config.js
@@ -9,4 +9,18 @@ module.exports = {
       },
     ],
   ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
+    },
+  },
 };
